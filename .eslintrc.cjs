0820d61ba3a927601d00c31b52cfebd76bd33123
abaduna~ <artuelrey@gmail.com>
2024-03-20 14:18:13 -0300

    chore: part 0

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,32 +1,30 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": "standard-with-typescript", // config propia
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "ignorePatterns": [".eslintrc.cjs", "dist", "vite.config.ts", "commitlint.config.cjs"],
-    "plugins": ["simple-import-sort"],
-    "rules": {
-        "simple-import-sort/imports": "error",
-        "@typescript-eslint/no-explicit-any": "error",
-        "@typescript-eslint/space-before-function-paren": "off"
-
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: 'standard-with-typescript', // config propia
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script'
+      }
     }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+
+  ignorePatterns: [],
+  plugins: ['simple-import-sort'],
+  rules: {
+    'simple-import-sort/imports': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/space-before-function-paren': 'off'
+  }
 }
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+function suma(num1:number,num2:number):number {
+    return num1 + num2
+}
