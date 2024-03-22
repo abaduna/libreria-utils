66d054e1c036fd8b122f18b8c85f1ac494b175e5
abaduna <artuelrey@gmail.com>
2024-03-22 14:18:15 -0300

    feat: generator dicionary

--- NodeTs/src/index.js
+++ NodeTs/src/index.js
@@ -1,81 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.moveNPalabra = exports.converNumberToString = exports.moveNPlace = exports.converStrinToNumber = void 0;
-var abcInNumber = [
-    ["a", 1],
-    ["b", 2],
-    ["c", 3],
-    ["d", 4],
-    ["e", 5],
-    ["f", 6],
-    ["g", 7],
-    ["h", 8],
-    ["i", 9],
-    ["j", 10],
-    ["k", 11],
-    ["m", 12],
-    ["n", 13],
-    ["o", 14],
-    ["p", 15],
-    ["q", 16],
-    ["r", 17],
-    ["s", 18],
-    ["t", 19],
-    ["u", 20],
-    ["v", 21],
-    ["w", 22],
-    ["x", 23],
-    ["y", 24],
-    ["z", 25],
-];
-function converStrinToNumber(s) {
-    var totalValue = 0;
-    var lowerCaseS = s.toLowerCase();
-    for (var _i = 0, abcInNumber_1 = abcInNumber; _i < abcInNumber_1.length; _i++) {
-        var later = abcInNumber_1[_i];
-        if (later[0] === lowerCaseS) {
-            totalValue = later[1];
-        }
-        return totalValue;
+exports.generatorDicionary = void 0;
+function generatorDicionary(word) {
+    var dicionary = [];
+    for (var i = 0; i < word.length * 2; i++) {
+        var numerRandon = Math.floor(Math.random() * (25 - 1 + 1)) + 1;
+        dicionary.push(numerRandon);
     }
+    return dicionary;
 }
-exports.converStrinToNumber = converStrinToNumber;
-function moveNPlace(s, n) {
-    var numberOriginal = converStrinToNumber(s);
-    //nunca va a pasar
-    if (numberOriginal === undefined) {
-        return;
-    }
-    return numberOriginal + n;
-}
-exports.moveNPlace = moveNPlace;
-function converNumberToString(n) {
-    var letraCodificada = "";
-    for (var _i = 0, abcInNumber_2 = abcInNumber; _i < abcInNumber_2.length; _i++) {
-        var later = abcInNumber_2[_i];
-        if (later[1] === n) {
-            letraCodificada = later[0];
-        }
-    }
-    return letraCodificada;
-}
-exports.converNumberToString = converNumberToString;
-function moveNPalabra(palabra, dicionario) {
-    var palabraToArry = palabra.split("");
-    var plabraCodificada = [];
-    for (var i = 0; i < palabraToArry.length; i++) {
-        console.log(palabraToArry[i], dicionario[i]);
-        var numeberToPalabra = moveNPlace(palabraToArry[i], dicionario[i]);
-        if (typeof numeberToPalabra === 'undefined') {
-            return;
-        }
-        var letraCodificada = converNumberToString(numeberToPalabra);
-        plabraCodificada.push(letraCodificada);
-    }
-    return plabraCodificada.join("");
-}
-exports.moveNPalabra = moveNPalabra;
-console.log(moveNPalabra("aaa", [24, 23, 22]));
-// console.log(`converStrinToNumber`,converStrinToNumber("a"));
-// console.log(`moveNPlace`,moveNPlace("a",1));
-// console.log("converNumberToString",converNumberToString(1));
+exports.generatorDicionary = generatorDicionary;
+console.log(generatorDicionary("artu"));
--- NodeTs/src/index.ts
+++ NodeTs/src/index.ts
@@ -1,78 +1,13 @@
-type AbcNumberPair = [string, number];
-const abcInNumber:AbcNumberPair[]= [
-    ["a", 1],
-    ["b", 2],
-    ["c", 3],
-    ["d", 4],
-    ["e", 5],
-    ["f", 6],
-    ["g", 7],
-    ["h", 8],
-    ["i", 9],
-    ["j", 10],
-    ["k", 11],
-    ["m", 12],
-    ["n", 13],
-    ["o", 14],
-    ["p", 15],
-    ["q", 16],
-    ["r", 17],
-    ["s", 18],
-    ["t", 19],
-    ["u", 20],
-    ["v", 21],
-    ["w", 22],
-    ["x", 23],
-    ["y", 24],
-    ["z", 25],
-  ]
-export function converStrinToNumber(s:string) {
-   
-  let totalValue:number = 0;
-  const lowerCaseS = s.toLowerCase();
-  for (const later of abcInNumber) {
-    if (later[0] ===lowerCaseS) {
-      totalValue = later[1]
-    }
+export function generatorDicionary(word:string) {
 
-  return totalValue
-}
-
-}
-export function moveNPlace (s:string,n:number){
-  
- const numberOriginal:number | undefined= converStrinToNumber(s)
- //nunca va a pasar
- if (numberOriginal === undefined) {
-    return 
+  let dicionary = []
+  for (let i = 0; i < word.length*2; i++) {
+   let numerRandon =Math.floor(Math.random() * (25 - 1 + 1)) + 1
+    dicionary.push(numerRandon)
+    
   }
-  return numberOriginal + n 
-}
-export function converNumberToString(n:number) {
-  let letraCodificada:string = "";
-  for (const later of abcInNumber) {
-    if (later[1] === n) {
-      letraCodificada = later[0]
-    }}
-  return letraCodificada
-}
-export function moveNPalabra(palabra:string,dicionario:number[]) {
-  const palabraToArry = palabra.split("")
-  const plabraCodificada:string[] = []
-    for (let i = 0; i < palabraToArry.length; i++) {
-        console.log(palabraToArry[i],dicionario[i]);
-        
-     const numeberToPalabra= moveNPlace(palabraToArry[i],dicionario[i])
-     if (typeof numeberToPalabra === 'undefined') {
-      return
-     }
-     const letraCodificada = converNumberToString(numeberToPalabra)
-     plabraCodificada.push(letraCodificada)
-    }
-    return plabraCodificada.join("")
+
+  return dicionary
 }
 
-console.log(moveNPalabra("aaa", [24, 23, 22]));
-// console.log(`converStrinToNumber`,converStrinToNumber("a"));
-// console.log(`moveNPlace`,moveNPlace("a",1));
-// console.log("converNumberToString",converNumberToString(1));
+console.log(generatorDicionary("artu"));
