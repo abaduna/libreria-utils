a77828726919d56ac90d4c499fe71cca1044ec57
abaduna <artuelrey@gmail.com>
2024-03-25 10:19:51 -0300

    chore: part 3

--- src/__test__/TransformWordinWordCodifig.test.ts
+++ src/__test__/TransformWordinWordCodifig.test.ts
@@ -1,16 +1,19 @@
 import { describe, expect, test } from 'vitest'
+import { moveNWord } from '../TransformWordinWordCodifig';
 
-import {moveNPalabra} from "../index"
+
 
 describe('moveNPalabra', () => {
     // @ts-expect-error should accept any value
     it('debería transformar [a,a,a] y [1,1,1] en [b,b,b]', () => {
-      const resultado = moveNPalabra("aaa", [1, 1, 1]);
+      const resultado = moveNWord("aaa", [1, 1, 1]);
       expect(resultado).toEqual( "bbb" );
     });
     // @ts-expect-error should accept any value
     it('debería transformar [a,a,a] y [1,1,1] en [b,b,b]', () => {
-        const resultado = moveNPalabra("aaa", [24, 23, 22]);
+        const resultado = moveNWord("aaa", [24, 23, 22]);
         expect(resultado).toEqual( "zyx" );
       });
+      
   });
+  
---
+++ src/__test__/generatorDicionary.test.ts
@@ -0,0 +1,4 @@
+
+import { describe, expect, test } from 'vitest'
+import { generatorDicionary } from '../generatorDicionary';
+
