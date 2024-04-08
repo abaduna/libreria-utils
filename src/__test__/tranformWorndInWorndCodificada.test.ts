89fa57334556e1e2c81a7ca7daa795cf7bc9b418
abaduna~ <artuelrey@gmail.com>
2024-04-08 09:59:11 -0300

    test: mutation test

--- src/__test__/tranformWorndInWorndCodificada.test.ts
+++ src/__test__/tranformWorndInWorndCodificada.test.ts
@@ -7,4 +7,13 @@ describe('moveNPalabra', () => {
     const result = moveNWord('aaa', [1, 1, 1])
     expect(result).toEqual('bbb')
   })
+  it(' Transform [] y [1,1,1] in [bbb]', () => {
+    const result = moveNWord('', [1])
+    expect(result).toEqual('')
+  })
+  it(' Transform [1,1,1] y [1,1,1] in [bbb]', () => {
+    // @ts-expect-error we want to test the undefined case
+    const result = moveNWord(1, [1])
+    expect(result).toEqual('')
+  })
 })
