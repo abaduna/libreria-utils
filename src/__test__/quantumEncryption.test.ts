7891ad353e73fc4a537056053c7402ccab4b0dfc
abaduna~ <artuelrey@gmail.com>
2024-04-08 15:22:55 -0300

    test: test decrypt

--- src/__test__/quantumEncryption.test.ts
+++ src/__test__/quantumEncryption.test.ts
@@ -4,12 +4,14 @@ import { quantumEncryption } from '../quantumEncryption'
 
 describe('moveNPalabra', () => {
   it(' Transform [a,a,a] y [1,1,1] in [bbb]', () => {
-    // @ts-expect-error we want to test the undefined case
-    const result = quantumEncryption('secreto', [1, 1, 1, 1, 1, 1, 1], 'decrypt')
+    const result = quantumEncryption(
+      'secreto',
+      [1, 1, 1, 1, 1, 1, 1],
+      'decrypt'
+    )
     expect(result).toEqual('rdbqdsn')
   })
   it(' Transform [] y [1,1,1] in [bbb]', () => {
-    // @ts-expect-error we want to test the undefined case
     const result = quantumEncryption('', [1], 'decrypt')
     expect(result).toEqual('')
   })
@@ -19,12 +21,14 @@ describe('moveNPalabra', () => {
     expect(result).toEqual('')
   })
   it(' Transform [1,1,1] y [1,1,1] in [bbb]', () => {
-    // @ts-expect-error we want to test the undefined case
-    const result = quantumEncryption('rdbqdsn', [1, 1, 1, 1, 1, 1, 1], 'encrypt')
+    const result = quantumEncryption(
+      'rdbqdsn',
+      [1, 1, 1, 1, 1, 1, 1],
+      'encrypt'
+    )
     expect(result).toEqual('secreto')
   })
   it(' Transform [] y [1,1,1] in [bbb]', () => {
-    // @ts-expect-error we want to test the undefined case
     const result = quantumEncryption('', [1], 'encrypt')
     expect(result).toEqual('')
   })
@@ -34,7 +38,6 @@ describe('moveNPalabra', () => {
     expect(result).toEqual('')
   })
   it(' Transform [1,1,1] y [1,1,1] in [bbb]', () => {
-    // @ts-expect-error we want to test the undefined case
     const result = quantumEncryption('rdbqdsn', [1, 1, 1, 1, 1, 1, 1], 'blabla')
     expect(result).toEqual('')
   })
